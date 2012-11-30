b25758f0b4f90660ae9b33c6f98bb222b5392d3b
Benjamin Kampmann <ben.kampmann@gmail.com>
2012-11-30 18:48:17 +0100

    merge everything into backbone app

--- static/js/main.js
+++ static/js/main.js
@@ -3,27 +3,92 @@ console.log($);
 
 $(function() {
 
-	var HomeView = Backbone.View.extend({
+  var TmplView = Backbone.View.extend({
+      render:function () {
+          $(this.el).html(this.template());
+          return this;
+      }
+  });
 
-	template: _.template($('#tmpl-main').html()),
+	var HomeView = TmplView.extend({
+     template: _.template($('#tmpl-main').html())
+  });
 
-    render:function () {
-        $(this.el).html(this.template());
-        return this;
+  var BooksView = TmplView.extend({
+     template: _.template($('#tmpl-books').html())
+  });
+
+  var BooksByView = TmplView.extend({
+     template: _.template($('#tmpl-books-by').html())
+  });
+
+  var BookView = TmplView.extend({
+     template: _.template($('#tmpl-book').html())
+  });
+
+  var MainNavView = Backbone.View.extend({
+    select: function(to_select) {
+      console.log(this);
+      var me = this.$el;
+      me.find("li").removeClass("active");
+      me.find("#" + to_select).addClass("active");
     }
 
-});
+  });
+
 
   var Router = Backbone.Router.extend({
 
     routes: {
-        "": "home"
+        "": "home",
+        // Books
+        "books/by/:user": "books_by",
+        "books/:book": "book",
+        "books/": "books"
     },
 
     el: $("body"),
 
     initialize: function() {
       this.main = $('#main');
+      this.mainNavView = new MainNavView({el: $('#main-nav')});
+    },
+
+    books: function () {
+        // Since the home view never changes, we instantiate it and render it only once
+        if (!this.booksView) {
+            this.booksView = new BooksView();
+            this.booksView.render();
+        } else {
+            this.booksView.delegateEvents(); // delegate events when the view is recycled
+        }
+        this.main.html(this.booksView.el);
+        this.mainNavView.select('books-menu');
+    },
+
+
+    books_by: function (user) {
+        // Since the home view never changes, we instantiate it and render it only once
+        if (!this.booksByView) {
+            this.booksByView = new BooksByView();
+            this.booksByView.render();
+        } else {
+            this.booksByView.delegateEvents(); // delegate events when the view is recycled
+        }
+        this.main.html(this.booksByView.el);
+        this.mainNavView.select('books-menu');
+    },
+
+    book: function (book) {
+        // Since the home view never changes, we instantiate it and render it only once
+        if (!this.bookView) {
+            this.bookView = new BookView();
+            this.bookView.render();
+        } else {
+            this.bookView.delegateEvents(); // delegate events when the view is recycled
+        }
+        this.main.html(this.bookView.el);
+        this.mainNavView.select('books-menu');
     },
 
     home: function () {
@@ -35,7 +100,7 @@ $(function() {
             this.homeView.delegateEvents(); // delegate events when the view is recycled
         }
         this.main.html(this.homeView.el);
-        // this.headerView.select('home-menu');
+        this.mainNavView.select('home-menu');
     }
 
   });
